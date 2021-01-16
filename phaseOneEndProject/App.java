99398fb4e24be8644a134ab5b66b76a257e1207c
DAGroveGH <dagrove7@gmail.com>
2021-01-16 15:36:51 -0500

    Initial commit for LockedMe.com framework

---
+++ phaseOneEndProject/App.java
@@ -0,0 +1,33 @@
+package phaseOneEndProject;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static java.lang.System.exit;
+
+public class App {
+    public static void main(String[] args){
+
+    }
+    public static void printWelcome() {}
+    public static void printMain() {
+    }
+    public static void printFiles(){}
+    public static boolean addFile(String fileName) {
+        return true;
+    }
+    public static boolean deleteFile(String fileName) {
+        return true;
+    }
+    public static boolean searchFile(String fileName) {
+        return true;
+    }
+}
